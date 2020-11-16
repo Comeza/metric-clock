7ac696991751ea79130efd41c635aa15919671dd
Comeza <mccomesa@gmail.com>
2020-11-17 00:26:36 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'index.sass';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component<{}, { timeInSeconds: string }> {
+  state = { timeInSeconds: this.getMetricTime() };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        timeInSeconds: this.getMetricTime(),
+      });
+    }, 200);
+  }
+
+  mod(divisee: number, base: number) {
+    return Math.round(divisee - Math.floor(divisee / base) * base);
+  }
+
+  getMetricTime(): string {
+    var dte = new Date();
+    var hrs = dte.getHours();
+    var min = dte.getMinutes();
+    var sec = dte.getSeconds();
+    var mil = dte.getMilliseconds();
+    var totalmil = mil + sec * 1000 + min * 1000 * 60 + hrs * 1000 * 60 * 60;
+    var totalmetsecs = parseInt(`${totalmil / 864}`);
+    var methrs = parseInt(`${totalmetsecs / 10000}`);
+    var metmin = parseInt(`${this.mod(totalmetsecs, 10000) / 100}`);
+    var metsec = this.mod(totalmetsecs, 100);
+
+    return [methrs, metmin, metsec].map((n) => (n <= 9 ? `0${n}` : `${n}`)).join(':');
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <div>{this.state.timeInSeconds}</div>
+      </div>
+    );
+  }
 }
-
-export default App;
